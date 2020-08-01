98db8931140b0439a5572fbc2a99b16f1719f0cd
Shubham Kumar <shubham.snu@gmail.com>
2020-08-01 22:48:08 +0530

    Added Blueprint

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// BUDGET CONTROLLER
 var budgetController = (function(){
     var x = 23;
     var add = function(a){
@@ -11,15 +12,24 @@ var budgetController = (function(){
     }
 })();
 
+// BUDGET UI CONTROLLER
 var UIController = (function(){
 
 })();
 
+// GLOBAL APP CONTROLLER
 var AppController = (function(budgetCtrl, UICtrl){
- var z = budgetCtrl.publicTest(5);
- return{
-     anotherPublic: function(){
-         console.log(z);
-     }
- }
+ 
+    var ctrlAddItem = function(){
+        // 1. Get input
+        // 2. Add item to budget
+        // 3. Add item to UI
+        // Display the budget on UI
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress',function(event){
+        if(event.keyCode === 13 || event.which === 13)
+           ctrlAddItem();
+    });
 })(budgetController, UIController);
