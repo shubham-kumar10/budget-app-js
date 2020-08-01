c6693d26405a4e526e6bb64379de8b1b0e2978a7
= <shubham.snu@gmail.com>
2020-08-01 23:16:48 +0530

    Added Input JS functions

--- app.js
+++ app.js
@@ -14,7 +14,25 @@ var budgetController = (function(){
 
 // BUDGET UI CONTROLLER
 var UIController = (function(){
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: 'add__value',
+        inputBtn: 'add__btn'
+    }
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.description).value,
+                value: document.querySelector(DOMstrings.value).value
+            }
+        },
 
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    }
 })();
 
 // GLOBAL APP CONTROLLER
@@ -22,14 +40,19 @@ var AppController = (function(budgetCtrl, UICtrl){
  
     var ctrlAddItem = function(){
         // 1. Get input
+
+        var input  = UIController.getInput();
+        console.log(input);
         // 2. Add item to budget
         // 3. Add item to UI
         // Display the budget on UI
     }
+    var DOMstrings = getDOMstrings();
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOMstrings.inputBtn).addEventListener('click', ctrlAddItem);
     document.addEventListener('keypress',function(event){
         if(event.keyCode === 13 || event.which === 13)
            ctrlAddItem();
     });
+
 })(budgetController, UIController);
